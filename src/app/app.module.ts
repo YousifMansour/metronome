6567abf55715d670b74d3022bda910e8f2faa4a3
Yousif <yousifyacoub1997@gmail.com>
2018-10-10 22:43:08 +0300

    Overhauled design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';  //<<<< import it here
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -9,8 +14,10 @@ import {TheMetronomeComponent} from './the-metronome/the-metronome.component';
 
 @NgModule({
   declarations: [AppComponent, TheMetronomeComponent],
-  imports:
-      [BrowserModule, MatSliderModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule, MatSliderModule, FormsModule, BrowserAnimationsModule,
+    MatButtonModule, MatCardModule, MatCheckboxModule, MatExpansionModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/the-metronome/the-metronome.component.ts
+++ src/app/the-metronome/the-metronome.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import Pizzicato from 'node_modules/pizzicato/distr/Pizzicato.js';
 
-
-
 @Component({
   selector: 'app-the-metronome',
   templateUrl: './the-metronome.component.html',
@@ -13,21 +11,23 @@ export class TheMetronomeComponent implements OnInit {
   sine1: any;
   sine2: any;
   tempo: number = 200;
-  beatsPerBar: number = 4;
+  beatsPerBar: number = 8;
   clicksPerBeat: number = 1;
   loopID: any;
   counter: number;
-  volume: number = 0.8;
+  volume: number = 1;
+
+  // sliders
+  numberOfTempoSteps: number = 5;
 
   ngOnInit() {
+    // initializes the sounds needed for playback
     this.loadSound();
   }
 
   loadSound() {
+    // server sounds currently not being used
     // this.sine1 = new Pizzicato.Sound('http://45.32.150.44:8080/sine1.wav');
-
-    // this.sine2 = new Pizzicato.Sound('http://45.32.150.44:8080/sine2.wav');
-
     this.sine1 =
         new Pizzicato.Sound({source: 'wave', options: {frequency: 880}});
 
@@ -35,12 +35,16 @@ export class TheMetronomeComponent implements OnInit {
         new Pizzicato.Sound({source: 'wave', options: {frequency: 440}});
   }
 
+  // this is where the metronome looping starts. its the only place where sound
+  // is gonna come out from
   loop() {
     if (this.loopID == null) {
       this.sine1.stop();
       this.sine2.stop();
 
       let beatLength = (60 / (this.tempo * this.clicksPerBeat));
+      // this set interval function is how the metronome works as a metronome
+      // given a tempo.
       this.loopID = setInterval(function() {
         if (this.counter == null) this.counter = 0;
         this.counter++;
@@ -79,6 +83,7 @@ export class TheMetronomeComponent implements OnInit {
     }
   }
 
+  // stops sound playing using the clear interval method
   stop() {
     clearInterval(this.loopID);
     this.counter = null;
@@ -88,12 +93,43 @@ export class TheMetronomeComponent implements OnInit {
     this.sine2.stop();
   }
 
+  // this is called when options are changed to update the metronome
   setOptions(tempo: number, beatsPerBar, clicksPerBeat) {
     if (tempo >= 1 && tempo) this.tempo = tempo;
 
+    if (beatsPerBar && beatsPerBar >= 1) this.beatsPerBar = beatsPerBar;
 
-    if (beatsPerBar >= 1 && beatsPerBar) this.beatsPerBar = beatsPerBar;
+    if (clicksPerBeat && clicksPerBeat >= 1) this.clicksPerBeat = clicksPerBeat;
+  }
 
-    if (clicksPerBeat >= 1 && clicksPerBeat) this.clicksPerBeat = clicksPerBeat;
+  changeTempo(tempo: number) {
+    this.tempo = tempo;
+    if (this.loopID) {
+      this.stop();
+      this.loop();
+    }
+  }
+
+  changeBeatsPerBar(beatsPerBar: number) {
+    this.beatsPerBar = beatsPerBar;
+    if (this.loopID) {
+      this.stop();
+      this.loop();
+    }
+  }
+
+  changeClicksPerBeat(clicksPerBeat: number) {
+    this.clicksPerBeat = clicksPerBeat;
+    if (this.loopID) {
+      this.stop();
+      this.loop();
+    }
+  }
+
+  toggleFinerSteps() {
+    if (this.numberOfTempoSteps == 5)
+      this.numberOfTempoSteps = 1;
+    else
+      this.numberOfTempoSteps = 5;
   }
 }
